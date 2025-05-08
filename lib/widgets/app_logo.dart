d632839671ed4477dac65cb6cbecb593b88f3b73
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-05-09 04:01:41 +0500

    code refactor

---
+++ lib/widgets/app_logo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppLogo extends StatelessWidget {
+  const AppLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+        "assets/image/logo.png",
+        height: 200,
+      ),
+    );
+  }
+}
